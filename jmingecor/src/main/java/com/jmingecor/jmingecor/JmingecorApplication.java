885704cd2563ec66cf125fbcce252941a676c070
People2002 <a.zelaya.p.j@gmail.com>
2022-10-10 22:27:43 -0500

    prueba

---
+++ jmingecor/src/main/java/com/jmingecor/jmingecor/JmingecorApplication.java
@@ -0,0 +1,13 @@
+package com.jmingecor.jmingecor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmingecorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmingecorApplication.class, args);
+	}
+
+}
---
+++ jmingecor/src/test/java/com/jmingecor/jmingecor/JmingecorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmingecor.jmingecor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmingecorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
