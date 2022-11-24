ecfca509ba0a6c9dcfbb30ec01296463c810bb35
BrunoApolaya30 <84215663+BrunoApolaya30@users.noreply.github.com>
2022-11-24 09:19:29 -0500

    Add files via upload

---
+++ jmingecor/src/main/java/com/jmingecor/jmingecor/model/dao/IValeSalidaDAO.java
@@ -0,0 +1,9 @@
+package com.jmingecor.jmingecor.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jmingecor.jmingecor.model.entity.ValeSalida;
+
+public interface  IValeSalidaDAO extends CrudRepository<ValeSalida, Long>{
+    
+}
