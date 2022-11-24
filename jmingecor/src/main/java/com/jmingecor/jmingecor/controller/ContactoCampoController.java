842fa1ea96bc40bef5e24cf7736cef56acbb643a
BrunoApolaya30 <84215663+BrunoApolaya30@users.noreply.github.com>
2022-11-24 09:09:46 -0500

    Add files via upload

--- jmingecor/src/main/java/com/jmingecor/jmingecor/controller/ContactoCampoController.java
+++ jmingecor/src/main/java/com/jmingecor/jmingecor/controller/ContactoCampoController.java
@@ -1,40 +1,76 @@
 package com.jmingecor.jmingecor.controller;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jmingecor.jmingecor.model.entity.ContactoCampo;
 import com.jmingecor.jmingecor.model.service.IContactoCampoService;
 
 @Controller
+@RequestMapping("/contactocampo")
 public class ContactoCampoController {
+
     @Autowired
     private IContactoCampoService contactoCampoService;
 
-    
-    public String index(Model model) {
+    @RequestMapping("/")
+    public String index(Model model, @RequestParam Map<String, Object> params){
+        
+        int page = params.get("page")  != null ? (Integer.valueOf(params.get("page").toString()) -1 ) : 0;
+        int numPage = params.get("numPage") != null ? (Integer.valueOf(params.get("numPage").toString())) : 5;
+
+        PageRequest pageRequest = PageRequest.of(page,numPage);
+        Page<ContactoCampo> pageContactoCampo = contactoCampoService.getAll(pageRequest);
+        
+        int totalPage = pageContactoCampo.getTotalPages();
+
+        if(totalPage > 0){
+            List<Integer> pages = IntStream.rangeClosed(1, totalPage).boxed().collect(Collectors.toList());
+            model.addAttribute("pages", pages);
+        }
+
         ContactoCampo objContactoCampo = new ContactoCampo();
-        model.addAttribute("contactoCampo", objContactoCampo);
-        model.addAttribute("listaContactosCampo", contactoCampoService.cargarContactosCampo());
-        return "contactocampo/main";
+        model.addAttribute("contactoCampo",objContactoCampo);
+        model.addAttribute("listaContactoCampo", contactoCampoService.cargarContactosCampo());
+        model.addAttribute("list", pageContactoCampo.getContent());
+        model.addAttribute("current", page+1);
+        model.addAttribute("next", page+2);
+        model.addAttribute("prev", page);
+        model.addAttribute("last", totalPage);
+        model.addAttribute("numPage", numPage);
+        return "contactoCampo/main";
     }
 
-    public String update(@PathVariable(value = "id") Long id, Model model) {
-        ContactoCampo objContactoCampo = contactoCampoService.buscarContactoCampo(id);
+    @RequestMapping("/editar/{id_contacto_campo}")
+    public String update(@PathVariable(value = "id_contacto_campo") Long id_contacto_campo, Model model) {
+        ContactoCampo objContactoCampo = contactoCampoService.buscarContactoCampo(id_contacto_campo);
         model.addAttribute("contactoCampo", objContactoCampo);
-        model.addAttribute("listaContactosCampo", contactoCampoService.cargarContactosCampo());
-        return "contactocampo/main";
-    }
-
-    public String delete(@PathVariable(value = "id") Long id, Model model) {
-        contactoCampoService.eliminarContactoCampo(id);
-        return "redirect:/contactocampo/";
+        model.addAttribute("listaContactoCampo", contactoCampoService.cargarContactosCampo());
+        return "contactoCampo/main";   
     }
     
-    public String store(ContactoCampo contactoCampo, Model model) {
-        contactoCampoService.guardarContactoCampo(contactoCampo);
-        return "redirect:/contactocampo/";
+    @RequestMapping("/eliminar/{id_contacto_campo}")
+    public String delete(@PathVariable(value = "id_contacto_campo") Long id_contacto_campo, Model model) {
+        contactoCampoService.eliminarContactoCampo(id_contacto_campo);
+        return "redirect:/contactoCampo/";
     }
+
+    @RequestMapping(value="/form", method = RequestMethod.POST)
+    public String store(ContactoCampo contactoCampo, Model model){
+        contactoCampoService.guardarContactoCampo(contactoCampo);
+        return "redirect:/contactoCampo/";
+    }
+
 }
