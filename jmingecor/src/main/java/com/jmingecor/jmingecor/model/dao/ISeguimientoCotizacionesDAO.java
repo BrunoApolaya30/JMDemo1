ab784aa0dc7842f2e1b0fcded4cb9a3047cad6a9
BrunoApolaya30 <84215663+BrunoApolaya30@users.noreply.github.com>
2022-11-24 23:06:14 -0500

    Add files via upload

---
+++ jmingecor/src/main/java/com/jmingecor/jmingecor/model/dao/ISeguimientoCotizacionesDAO.java
@@ -0,0 +1,9 @@
+package com.jmingecor.jmingecor.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jmingecor.jmingecor.model.entity.SeguimientoCotizaciones;
+
+public interface ISeguimientoCotizacionesDAO extends CrudRepository<SeguimientoCotizaciones,Long>{
+    
+}
---
+++ jmingecor/src/main/java/com/jmingecor/jmingecor/model/dao/ISeguimientoOTDAO.java
@@ -0,0 +1,9 @@
+package com.jmingecor.jmingecor.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jmingecor.jmingecor.model.entity.SeguimientoOT;
+
+public interface ISeguimientoOTDAO extends CrudRepository<SeguimientoOT,Long>{
+    
+}
