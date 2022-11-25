55b7c08bdb9547ae9252fadded124f6bd7b490c1
BrunoApolaya30 <84215663+BrunoApolaya30@users.noreply.github.com>
2022-11-24 23:05:31 -0500

    Add files via upload

---
+++ jmingecor/src/main/java/com/jmingecor/jmingecor/model/repository/ISeguimientoControlRepository.java
@@ -0,0 +1,9 @@
+package com.jmingecor.jmingecor.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jmingecor.jmingecor.model.entity.SeguimientoControl;
+
+public interface ISeguimientoControlRepository extends JpaRepository<SeguimientoControl,Long> {
+    
+}
